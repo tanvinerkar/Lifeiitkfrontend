3a94bfd47ecc185e896c871e2d20ea550636f6c7
tanvinerkar <tanvinerkar517@gmail.com>
2019-06-21 22:29:48 +0530

    Create Profile Page with personal details
    Generate profile component
    
    Use Grid Component to render profile UI page
    
    Add Pors

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from "react";
-// import logo from "./logo.svg";
 import "./App.css";
 import SideBar from "./components/sidebar/sidebar";
 import TopBar from "./components/topbar/topbar";
